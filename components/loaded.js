1d160ac0e83a7a999721875605f02820605d9ed3
ŊʂƓ PRIYANSHU <166146782+nsgpriyanshu@users.noreply.github.com>
2024-04-06 17:12:37 +0530

    Added important files
    Added important files and folders

---
+++ components/loaded.js
@@ -0,0 +1,22 @@
+// Wait for the document to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  // Add 'loaded' class to the card after a short delay
+  setTimeout(function () {
+    document.querySelector('.card').classList.add('loaded');
+  }, 750); // Adjusted delay for the card to 0.75 seconds (750 milliseconds)
+
+  // Add 'loaded' class to midcard after a delay
+  setTimeout(function () {
+    document.querySelector('.midcard').classList.add('loaded');
+  }, 2250); // Adjusted delay for midcard to 2.25 seconds (2250 milliseconds)
+
+  // Add 'loaded' class to midcard2 after a delay
+  setTimeout(function () {
+    document.querySelector('.midcard2').classList.add('loaded');
+  }, 3750); // Adjusted delay for midcard2 to 3.75 seconds (3750 milliseconds)
+
+  // Add 'loaded' class to midcard3 after a delay
+  setTimeout(function () {
+    document.querySelector('.midcard3').classList.add('loaded');
+  }, 5250); // Adjusted delay for midcard3 to 5.25 seconds (5250 milliseconds)
+});
