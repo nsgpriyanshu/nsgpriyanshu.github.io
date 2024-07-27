724f5fd9c38292250d413f3910ee21c34a08eef7
nsgpriyanshu <166146782+nsgpriyanshu@users.noreply.github.com>
2024-07-27 19:48:57 +0530

    chore(release): Stable version v1.3.0
    This version includes the following changes:
    
    - feat: add ns docs (83bac56)
    
    - feat: add new image and project info (3118c82)
    
    - ci: add code ql (850ae3f)
    
    - chore: update all the pics (6520657)
    
    - Merge branch 'main' of https://github.com/nsgpriyanshu/nsgpriyanshu.github.io (93d2067)
    
    - chore: add commit linting configuration (f3e1164)
    
    - chore: update package dependencies (97f757e)
    
    - style: update CSS files (896ede5)
    
    - fix: update index files (13b3a80)
    
    - fix: update component logic (1a58f98)
    
    Additionally, security changes include installing Express and fixing related security issues.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const path = require("path");
+const RateLimit = require("express-rate-limit");
+
 const app = express();
 const port = 3000;
+
+// Set up rate limiter: maximum of 100 requests per 15 minutes
+const limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+
+// Apply rate limiter to all requests
+app.use(limiter);
 
 app.use("/", express.static(path.join(__dirname, "./frontend")));
 
@@ -10,5 +21,6 @@ app.use(function (req, res) {
 });
 
 app.listen(port, () => {
-  console.log(`Sucessfully available at ${port}`);
+  console.log(`Successfully available at ${port}`);
 });
+
