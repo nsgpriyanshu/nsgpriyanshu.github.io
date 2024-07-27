13b3a802b03d77d44d67c442849d62349729df2c
nsgpriyanshu <166146782+nsgpriyanshu@users.noreply.github.com>
2024-07-27 15:09:28 +0530

    fix: update index files

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use('/', express.static(path.join(__dirname, './frontend')));
+app.use("/", express.static(path.join(__dirname, "./frontend")));
 
 app.use(function (req, res) {
   res.status(404).sendFile(path.join(__dirname, "./frontend/index.html"));
