f961c5dc985182d7dc8ff3d877219a27a7c2e576
nsgpriyanshu <166146782+nsgpriyanshu@users.noreply.github.com>
2024-08-30 20:11:58 +0530

    chore: update the tree

--- components/loaded.js
+++ components/loaded.js
@@ -20,8 +20,13 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".midcard4").classList.add("loaded");
   }, 5250); // Adjusted delay for midcard4 to 5.25 seconds (5250 milliseconds)
 
+   // Add 'loaded' class to midcard3 after a delay
+   setTimeout(function () {
+    document.querySelector(".midcard5").classList.add("loaded");
+  }, 6750); // Adjusted delay for midcard3 to 6.75 seconds (6750 milliseconds)
+
   // Add 'loaded' class to midcard3 after a delay
   setTimeout(function () {
     document.querySelector(".midcard3").classList.add("loaded");
-  }, 6750); // Adjusted delay for midcard3 to 6.75 seconds (6750 milliseconds)
+  }, 8250); // Adjusted delay for midcard3 to 6.75 seconds (6750 milliseconds)
 });
