679ae3ef00098bbac6cecb8920836bfc1d6769aa
nsgpriyanshu <166146782+nsgpriyanshu@users.noreply.github.com>
2024-10-22 16:27:38 +0530

    deps: add clifjumper and angularcommit

--- v2/next.config.mjs
+++ v2/next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true, 
-    },
-    typescript: {
-      ignoreBuildErrors: true, 
-    },
-  };
-  
-  export default nextConfig;
-  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+export default nextConfig
